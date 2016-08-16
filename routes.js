316e497caedc2cb37289bd911ba95f6ba1e35960
Eden Toledano <edentoledano@gmail.com>
2016-08-16 12:12:05 +0300

    visual fixes

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
-//  NOTE: SERVER FAILS to load this routs. for now server.js routs by itself.
 var requestHandler = require('./requestHandler');
 var multiparty = require('connect-multiparty'),
-    multipartyMiddleware = multiparty();
+    multipartyMiddleware = multiparty(); // allows to access req.files.file
 
 // here we define the routes:
 module.exports = function(app) {
--- server.js
+++ server.js
@@ -2,28 +2,12 @@
 // setup express app
 var express = require('express');
 var app = express();
-
 var logger = require('morgan');
 var port = process.env.PORT || 3000;
-
-app.use(logger('dev')); // for nice logs
-
-app.use(express.static(__dirname + '/public'));
-
-// note to self: external routs are not working for now, so i moved
-//  this into the server.js file. after everything works, fixs this!
-
 require('./routes')(app);
 
-
-// add multiparty module in order to access req.files.file
-
-// var multiparty = require('connect-multiparty'),
-//     multipartyMiddleware = multiparty();
-
-// var requestHandler = require('./requestHandler');
-
-// app.post('/api/upload', multipartyMiddleware, requestHandler.upload);
+app.use(logger('dev')); // for nice logs
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port);
 console.log('Listening on port ' + port);
